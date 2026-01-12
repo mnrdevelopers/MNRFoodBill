1a700f203b31c8054c4c1c1f333cc42325723bd6
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2026-01-12 22:23:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mnrfoodbill-v3.0.2'; // Change version to force update
+const CACHE_NAME = 'mnrfoodbill-v3.0.3'; // Change version to force update
 const BASE_PATH = '/MNRFoodBill/';
 const AUTH_CHECK_URL = '/MNRFoodBill/auth-check.html';
 
